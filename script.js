b74d27e1aed44ecd692b66b6fdbecfc9109aaf1b
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2023-09-18 18:29:28 -0400

    Added Buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+function buttonClickedFirst() {
+    alert("First Button clicked! This statement is printed.");
+}
+function buttonClickedSecond() {
+    alert("Second Button clicked! This statement is printed.");
+}
